6a17e1599176ffcc3aef163aa2e88883fafa6876
Annas <annaskherunas06@gmail.com>
2024-08-28 23:34:01 +0700

    randomize quotaVariables.deadlineDaysAmount

--- ChaosCompany/Scripts/Patches/TimeOfDayPatch.cs
+++ ChaosCompany/Scripts/Patches/TimeOfDayPatch.cs
@@ -16,7 +16,6 @@ static class TimeOfDayPatch
             return;
         }
 
-        __instance.quotaVariables.increaseSteepness = Random.Range(4, 7);
         __instance.quotaVariables.deadlineDaysAmount = Random.Range(4, 7);
         __instance.globalTimeSpeedMultiplier = Random.Range(0.25f, 1.0f);
     }
